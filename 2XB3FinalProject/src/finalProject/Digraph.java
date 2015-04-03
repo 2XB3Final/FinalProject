a716b2266dc15e48b4317666c022a32ebe666a5e
eric73847 <EricLeFort@Harriss-MacBook-Pro.local>
2015-04-03 16:31:44 -0400

    This should fix the weird <<<<HEAD thing

--- 2XB3FinalProject/src/finalProject/Digraph.java
+++ 2XB3FinalProject/src/finalProject/Digraph.java
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 package finalProject;
->>>>>>> Eric
 /*************************************************************************
  *  Compilation:  javac Digraph.java
  *  Execution:    java Digraph filename.txt
@@ -31,10 +28,7 @@ package finalProject;
 
 import java.util.InputMismatchException;
 import java.util.NoSuchElementException;
-<<<<<<< HEAD
-=======
 import java.util.Stack;
->>>>>>> Eric
 
 /**
  *  The <tt>Digraph</tt> class represents a directed graph of vertices
@@ -57,11 +51,7 @@ import java.util.Stack;
  *  @author Kevin Wayne
  */
 
-<<<<<<< HEAD
-public class Digraph {
-=======
 public class Digraph{
->>>>>>> Eric
     private final int V;
     private int E;
     private Bag<Integer>[] adj;
@@ -71,26 +61,15 @@ public class Digraph{
      * @param V the number of vertices
      * @throws java.lang.IllegalArgumentException if V < 0
      */
-<<<<<<< HEAD
-    public Digraph(int V) {
-=======
     public Digraph(int V){
->>>>>>> Eric
         if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
         this.V = V;
         this.E = 0;
         adj = (Bag<Integer>[]) new Bag[V];
-<<<<<<< HEAD
-        for (int v = 0; v < V; v++) {
-            adj[v] = new Bag<Integer>();
-        }
-    }
-=======
         for(int v = 0; v < V; v++){
             adj[v] = new Bag<Integer>();
         }
     }//Constructor
->>>>>>> Eric
 
     /**  
      * Initializes a digraph from an input stream.
@@ -101,19 +80,6 @@ public class Digraph{
      * @throws java.lang.IndexOutOfBoundsException if the endpoints of any edge are not in prescribed range
      * @throws java.lang.IllegalArgumentException if the number of vertices or edges is negative
      */
-<<<<<<< HEAD
-    public Digraph(In in) {
-        try {
-            this.V = in.readInt();
-            if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
-            adj = (Bag<Integer>[]) new Bag[V];
-            for (int v = 0; v < V; v++) {
-                adj[v] = new Bag<Integer>();
-            }
-            int E = in.readInt();
-            if (E < 0) throw new IllegalArgumentException("Number of edges in a Digraph must be nonnegative");
-            for (int i = 0; i < E; i++) {
-=======
     public Digraph(In in){
         try{
             this.V = in.readInt();
@@ -125,44 +91,20 @@ public class Digraph{
             int E = in.readInt();
             if(E < 0) throw new IllegalArgumentException("Number of edges in a Digraph must be nonnegative");
             for(int i = 0; i < E; i++){
->>>>>>> Eric
                 int v = in.readInt();
                 int w = in.readInt();
                 addEdge(v, w); 
             }
         }
-<<<<<<< HEAD
-        catch (NoSuchElementException e) {
-            throw new InputMismatchException("Invalid input format in Digraph constructor");
-        }
-    }
-=======
         catch(NoSuchElementException e){
             throw new InputMismatchException("Invalid input format in Digraph constructor");
         }
     }//Constructor
->>>>>>> Eric
 
     /**
      * Initializes a new digraph that is a deep copy of <tt>G</tt>.
      * @param G the digraph to copy
      */
-<<<<<<< HEAD
-    public Digraph(Digraph G) {
-        this(G.V());
-        this.E = G.E();
-        for (int v = 0; v < G.V(); v++) {
-            // reverse so that adjacency list is in same order as original
-            Stack<Integer> reverse = new Stack<Integer>();
-            for (int w : G.adj[v]) {
-                reverse.push(w);
-            }
-            for (int w : reverse) {
-                adj[v].add(w);
-            }
-        }
-    }
-=======
     public Digraph(Digraph G){
         this(G.V());
         this.E = G.E();
@@ -177,36 +119,17 @@ public class Digraph{
             }
         }
     }//Constructor
->>>>>>> Eric
         
     /**
      * Returns the number of vertices in the digraph.
      * @return the number of vertices in the digraph
      */
-<<<<<<< HEAD
-    public int V() {
-        return V;
-    }
-=======
     public int V(){ return V; }//V()
->>>>>>> Eric
 
     /**
      * Returns the number of edges in the digraph.
      * @return the number of edges in the digraph
      */
-<<<<<<< HEAD
-    public int E() {
-        return E;
-    }
-
-
-    // throw an IndexOutOfBoundsException unless 0 <= v < V
-    private void validateVertex(int v) {
-        if (v < 0 || v >= V)
-            throw new IndexOutOfBoundsException("vertex " + v + " is not between 0 and " + (V-1));
-    }
-=======
     public int E(){ return E; }//E()
 
     /**
@@ -217,7 +140,6 @@ public class Digraph{
         if (v < 0 || v >= V)
             throw new IndexOutOfBoundsException("vertex " + v + " is not between 0 and " + (V-1));
     }//validateVertex()
->>>>>>> Eric
 
     /**
      * Adds the directed edge v->w to the digraph.
@@ -225,20 +147,12 @@ public class Digraph{
      * @param w the head vertex
      * @throws java.lang.IndexOutOfBoundsException unless both 0 <= v < V and 0 <= w < V
      */
-<<<<<<< HEAD
-    public void addEdge(int v, int w) {
-=======
     public void addEdge(int v, int w){
->>>>>>> Eric
         validateVertex(v);
         validateVertex(w);
         adj[v].add(w);
         E++;
-<<<<<<< HEAD
-    }
-=======
     }//addEdge()
->>>>>>> Eric
 
     /**
      * Returns the vertices adjacent from vertex <tt>v</tt> in the digraph.
@@ -246,17 +160,10 @@ public class Digraph{
      * @param v the vertex
      * @throws java.lang.IndexOutOfBoundsException unless 0 <= v < V
      */
-<<<<<<< HEAD
-    public Iterable<Integer> adj(int v) {
-        validateVertex(v);
-        return adj[v];
-    }
-=======
     public Iterable<Integer> adj(int v){
         validateVertex(v);
         return adj[v];
     }//adj()
->>>>>>> Eric
 
     /**
      * Returns the number of directed edges incident from vertex <tt>v</tt>.
@@ -265,42 +172,24 @@ public class Digraph{
      * @param v the vertex
      * @throws java.lang.IndexOutOfBoundsException unless 0 <= v < V
      */
-<<<<<<< HEAD
-    public int outdegree(int v) {
-        validateVertex(v);
-        return adj[v].size();
-    }
-=======
     public int outdegree(int v){
         validateVertex(v);
         return adj[v].size();
     }//outdegree()
->>>>>>> Eric
 
     /**
      * Returns the reverse of the digraph.
      * @return the reverse of the digraph
      */
-<<<<<<< HEAD
-    public Digraph reverse() {
-        Digraph R = new Digraph(V);
-        for (int v = 0; v < V; v++) {
-            for (int w : adj(v)) {
-=======
     public Digraph reverse(){
         Digraph R = new Digraph(V);
         for (int v = 0; v < V; v++){
             for (int w : adj(v)){
->>>>>>> Eric
                 R.addEdge(w, v);
             }
         }
         return R;
-<<<<<<< HEAD
-    }
-=======
     }//reverse()
->>>>>>> Eric
 
     /**
      * Returns a string representation of the graph.
@@ -308,15 +197,6 @@ public class Digraph{
      * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,  
      *    followed by the <em>V</em> adjacency lists
      */
-<<<<<<< HEAD
-    public String toString() {
-        StringBuilder s = new StringBuilder();
-        String NEWLINE = System.getProperty("line.separator");
-        s.append(V + " vertices, " + E + " edges " + NEWLINE);
-        for (int v = 0; v < V; v++) {
-            s.append(String.format("%d: ", v));
-            for (int w : adj[v]) {
-=======
     @Override
     public String toString(){
         StringBuilder s = new StringBuilder();
@@ -325,30 +205,16 @@ public class Digraph{
         for(int v = 0; v < V; v++){
             s.append(String.format("%d: ", v));
             for(int w : adj[v]){
->>>>>>> Eric
                 s.append(String.format("%d ", w));
             }
             s.append(NEWLINE);
         }
         return s.toString();
-<<<<<<< HEAD
-    }
-=======
     }//toString()
->>>>>>> Eric
 
     /**
      * Unit tests the <tt>Digraph</tt> data type.
      */
-<<<<<<< HEAD
-    public static void main(String[] args) {
-        In in = new In(args[0]);
-        Digraph G = new Digraph(in);
-        StdOut.println(G);
-    }
-
-}
-=======
     public static void main(String[] args){
         In in = new In(args[0]);
         Digraph G = new Digraph(in);
@@ -356,4 +222,3 @@ public class Digraph{
     }//main()
 
 }//Digraph
->>>>>>> Eric
