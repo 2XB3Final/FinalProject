5abc9add0cb64be31cfbd8a7c2979113dda9ade6
Fiona Hu <fionahxt@hotmail.com>
2015-04-06 11:34:38 -0400

    Added heard, told, deadEnds and BinarySearch
    This heard and told returns the list of all people that heard and told
    the rumour

--- 2XB3FinalProject/src/GraphFinalProj/Applications.java
+++ 2XB3FinalProject/src/GraphFinalProj/Applications.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 public class Applications {
-    private ArrayList<Integer> sources;
+    private ArrayList<Integer> sources = new ArrayList();
+    private ArrayList<Integer> told = new ArrayList();
+    private ArrayList<Integer> heard = new ArrayList();
+    private ArrayList<Integer> deadEnds = new ArrayList();
     
     /**
      * Find all the sources by doing reverse postorder on the input graph
@@ -13,7 +16,6 @@ public class Applications {
      */
     public ArrayList<Integer> getSource(Digraph G) {
     	DepthFirstOrder dfs = new DepthFirstOrder(G);		// run dfs on the input graph G
-    	sources = new ArrayList();							// initializes an arraylist for storing the sources
     	Stack<Integer> revPost = dfs.reversePost();			// run reverse postorder on dfs
     	while (!revPost.empty()) {							// while the stack is not empty
     		if (G.getToList(revPost.peek()).isEmpty()) {	// peek at the next element on the stack, check its ToList
@@ -26,7 +28,6 @@ public class Applications {
     }
     
     /**
-     * 
      * @param G
      * @param i
      * @return the adjacency list for input i
@@ -36,12 +37,51 @@ public class Applications {
     }
     
     /**
-     * 
+     * @param G
+     * @return the ordered list of all the people that have told the rumour
+     */
+    public ArrayList<Integer> told(Digraph G) {
+    	for (int i = 0; i < G.getAdjList().length(); i++) {
+    		if (!G.getAdjList(i).isEmpty()) {
+    			told.add(i);
+    		}
+    	}
+    	return told;
+    }
+    
+    /**
      * @param G
      * @param i
      * @return the to list for input i
      */
     public Bag<Integer> heard(Digraph G, int i) {
     	return G.getToList(i);
+    }
+    
+    /**
+     * @param G
+     * @return the ordered list of all the people that have heard the rumour
+     */
+    public ArrayList<Integer> heard(Digraph G) {
+    	for (int i = 0; i < G.getToList().length(); i++) {
+    		if (!G.getToList(i).isEmpty()) {
+    			heard.add(i);
+    		}
+    	}
+    	return heard;
+    }
+    
+    /**
+     * @param G
+     * @return the ordered list of all the people that have heard the rumour but did not spread it
+     */
+    public ArrayList<Integer> deadEnd(Digraph G) {
+    	for (int i = 0; i < heard.size(); i++) {
+    		deadEnds.add(BinarySearch.rank(heard.get(i), told));
+    	}
+    	while (deadEnds.contains(-1)) {
+    		deadEnds.remove(-1);
+    	}
+    	return deadEnds;
     }
 }
