f9a0430af72ff0d24c4c9427f2e2a897ed7b613b
Fiona Hu <fionahxt@hotmail.com>
2015-04-06 11:41:05 -0400

    Added comments

--- 2XB3FinalProject/src/GraphFinalProj/Applications.java
+++ 2XB3FinalProject/src/GraphFinalProj/Applications.java
@@ -41,10 +41,10 @@ public class Applications {
      * @return the ordered list of all the people that have told the rumour
      */
     public ArrayList<Integer> told(Digraph G) {
-    	for (int i = 0; i < G.getAdjList().length(); i++) {
-    		if (!G.getAdjList(i).isEmpty()) {
-    			told.add(i);
-    		}
+    	for (int i = 0; i < G.getAdjList().length(); i++) {		// iterate through the adjacency list
+    		if (!G.getAdjList(i).isEmpty()) {					// if an element's adjacency list is not
+    			told.add(i);									// empty, it has told the rumour, add it
+    		}													// to the told list
     	}
     	return told;
     }
@@ -63,10 +63,10 @@ public class Applications {
      * @return the ordered list of all the people that have heard the rumour
      */
     public ArrayList<Integer> heard(Digraph G) {
-    	for (int i = 0; i < G.getToList().length(); i++) {
-    		if (!G.getToList(i).isEmpty()) {
-    			heard.add(i);
-    		}
+    	for (int i = 0; i < G.getToList().length(); i++) {		// iterate through the to list
+    		if (!G.getToList(i).isEmpty()) {					// if an element's to list is not
+    			heard.add(i);									// empty, it has heard the rumour, add it
+    		}													// to the heard list
     	}
     	return heard;
     }
@@ -75,13 +75,13 @@ public class Applications {
      * @param G
      * @return the ordered list of all the people that have heard the rumour but did not spread it
      */
-    public ArrayList<Integer> deadEnd(Digraph G) {
-    	for (int i = 0; i < heard.size(); i++) {
-    		deadEnds.add(BinarySearch.rank(heard.get(i), told));
-    	}
-    	while (deadEnds.contains(-1)) {
-    		deadEnds.remove(-1);
-    	}
+    public ArrayList<Integer> deadEnds(Digraph G) {
+    	for (int i = 0; i < heard.size(); i++) {				// iterate every element in the heard
+    		deadEnds.add(BinarySearch.rank(heard.get(i), told));// list through the told list, if it's
+    	}														// in heard but not told, that means 
+    	while (deadEnds.contains(-1)) {							// it's a dead end
+    		deadEnds.remove(-1);								// BinarySearch makes returns -1 to a
+    	}														// search miss, need to remove them
     	return deadEnds;
     }
 }
